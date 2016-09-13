e054fefed435084508536ec4ce76e5947a2e59c1
WillianTartaro <williantartaro@gmail.com>
2016-09-13 19:37:39 -0300

    Finalizado o #8, fase1 no caso.

--- lab134155/src/main/java/serv/Config.java
+++ lab134155/src/main/java/serv/Config.java
@@ -1,11 +1,57 @@
 package serv;
 
+import java.io.IOException;
+
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
 import javax.jms.JMSDestinationDefinition;
 import javax.jms.JMSDestinationDefinitions;
+import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.jms.Topic;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-@JMSDestinationDefinitions(value = { @JMSDestinationDefinition(name = "java:/queue/QueuePedido", interfaceName = "javax.jms.Queue", destinationName = "QueuePedido"), 
-									@JMSDestinationDefinition(name = "java:/topic/TopicVenda", interfaceName = "javax.jms.Topic", destinationName = "TopicVenda") })
+import entidades.Venda;
 
-public class Config {
+@WebServlet("/Config")
+public class Config extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private JMSContext context;
+
+	@Resource(lookup = "java:/queue/QueuePedido")
+	private Queue queue;
+
+	@Resource(lookup = "java:/topic/TopicVenda")
+	private Topic topic;
+	
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			boolean useTopic = req.getParameterMap().keySet().contains("topic");
+			final Destination destination = useTopic ? topic : queue;
+			Venda venda = new Venda();
+			ObjectMessage objectMessage = context.createObjectMessage();
+			objectMessage.setObject(venda);
+			context.createProducer().send(destination, objectMessage);
+
+		} catch (Throwable th) {
+			th.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+	
 }
